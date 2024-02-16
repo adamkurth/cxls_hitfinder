326f22f9e21f404a5f1cb9ae3bf3b3a117e0b2d9
Adam Kurth <amkurth@asu.edu>
2024-02-16 16:32:48 -0700

    working on .sh and .pdb reader

--- cnn/src/cnn.py
+++ cnn/src/cnn.py
@@ -9,24 +9,6 @@ from torchvision import transforms
 from sklearn.model_selection import train_test_split
 from collections import namedtuple
 
-class PeakImageDataset(Dataset):
-    def __init__(self, image_paths, label_paths, transform=None):
-        self.image_paths = image_paths
-        self.label_paths = label_paths 
-        self.transform = transform
-        
-    def __len__(self):
-        return len(self.image_paths)
-    
-    def __getitem__(self, idx):
-        with h5py.File(self.image_paths[idx], 'r') as f:
-            image = np.array(f['entry/data/data'])
-        with h5py.File(self.label_paths[idx], 'r') as f:
-            label = np.array(f['entry/data/data'])
-                
-        if self.transform:
-            image = self.transform(image)
-            label = self.transform(label)
-        
-        return image, label
-  
+from src.dataprep import PeakImageDataset, prep_data
+
+
