073613e9014b61453f9d3ed4b95907e1ee4d0577
Adam Kurth <amkurth@asu.edu>
2024-02-23 23:47:23 -0700

    implemented pkg/ import module

--- cnn/src/dataprep.py
+++ cnn/src/dataprep.py
@@ -10,8 +10,10 @@ from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
 from sklearn.model_selection import train_test_split
 from collections import namedtuple
-import classes as cl
-import functions as fn
+
+from pkg import models as m
+from pkg import classes as c
+from pkg import functions as f
 
 # this python file is for testing the classes and functions in the classes.py and functions.py files
 # supposed to load the data in preparation for the CNN model  
@@ -27,7 +29,7 @@ def main():
     data_preparation = cl.DataPreparation(paths, batch_size=5)
     paths.clean_sim() # moves all .err, .out, .sh files sim_specs 
     train_loader, test_loader = data_preparation.prep_data()
-    
+
     sim_dict = fn.sim_parameters(paths)
     print(sim_dict)
 
