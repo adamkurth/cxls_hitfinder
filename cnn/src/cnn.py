c6c36930b6080f022d89dcda500967a89d624d83
Adam Kurth <amkurth@asu.edu>
2024-02-23 19:09:07 -0700

    checkpoint, added reformat bash script

--- cnn/src/cnn.py
+++ cnn/src/cnn.py
@@ -12,7 +12,7 @@ paths = cl.PathManager()
 paths.clean_sim() # moves all .err, .out, .sh files sim_specs 
 
 dataset = cl.PeakImageDataset(paths=paths, transform=transforms.ToTensor(), augment=False)
-prep = cl.DataPreparation(paths, batch_size=32)
+prep = cl.DataPreparation(paths=paths, batch_size=32)
 
 """checks"""
 peak_paths = paths.__get_peak_images_paths__()
@@ -28,28 +28,25 @@ model_res50 = md.CustomResNet50(num_proteins=10, num_camlengths=3)
 
 water_background = dataset.__load_h5__(paths.__get_path__('water_background_h5'))
 
-dataset.__len__()
-dataset.__get_item__(0)
-dataset.__preview__(0)
 
-# # loss and optimizer
-# criteron = nn.CrossEntropyLoss()
-# optimizer = optim.Adam(model.parameters(), lr=0.001)
+"""loss and optimizer"""
+criteron = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model_res50.parameters(), lr=0.001)
 
-# # train 
-# num_epochs = 10
+"""train""" 
+num_epochs = 10
 
-# for epoch in range(num_epochs):
-#     model.train()
-#     running_loss = 0.0
-#     for images, labels in train_loader:
-#         optimizer.zero_grad()
-#         outputs = model(images)
-#         loss = criteron(outputs, labels)
-#         loss.backward()
-#         optimizer.step()
-#         running_loss += loss.item()
-#     print(f"Epoch {epoch+1}, Loss: {running_loss/len(train_loader)}")
+for epoch in range(num_epochs):
+    model_res50.train()
+    running_loss = 0.0
+    for images, labels in train_loader:
+        optimizer.zero_grad()
+        outputs = model_res50(images)
+        loss = criteron(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+    print(f"Epoch {epoch+1}, Loss: {running_loss/len(train_loader)}")
     
 
 
--- cnn/src/models.py
+++ cnn/src/models.py
@@ -6,6 +6,7 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms, models
+from torchvision.models.resnet import ResNet50_Weights
 from sklearn.model_selection import train_test_split
 from collections import namedtuple
 
@@ -23,7 +24,7 @@ class CustomResNet50(nn.Module):
         # num_classes = num_proteins, num_camlengths
         super(CustomResNet50, self).__init__()
         # load the pre-trained model
-        self.resnet = models.resnet50(weights=True)
+        self.resnet = models.resnet50(weights=ResNet50_Weights.IMAGENET1K_V1)
         num_ftrs = self.resnet.fc.in_features
         # replace the final fully connected layer
         self.resnet.fc = nn.Linear(num_ftrs, num_proteins + num_camlengths) # proteins + camlengths
