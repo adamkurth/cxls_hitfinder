25bace1c53fa4466104d2e05ce06827c5a733ef7
Adam Kurth <amkurth@asu.edu>
2024-03-21 08:52:05 -0700

    test/train class in utils

--- cnn/src/pkg/util.py
+++ cnn/src/pkg/util.py
@@ -7,6 +7,8 @@ from functools import lru_cache
 from skimage.feature import peak_local_max
 from torch.utils.data import DataLoader, Dataset
 from torchvision.transforms.functional import to_tensor
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
 import numpy as np
 
 def load_h5(file_path:str) -> np.ndarray:
@@ -322,4 +324,159 @@ class TransformToTensor:
             raise ValueError(f"Image has invalid dimensions: {image.shape}")
         image_tensor = torch.from_numpy(image).float().to(dtype=torch.float32)  # Convert to tensor with dtype=torch.float32
         return image_tensor # dimensions: C x H x W 
+    
+
+class TrainTestModels:
+    """ This class trains, tests, and plots the loss, accuracy, and confusion matrix of a model.
+        There are two methods for training: test_model_no_freeze and test_model_freeze.
+    """
+    def __init__(self, model, loader: list, criterion, optimizer, device, cfg: dict) -> None:
+        """ Takes the arguments for training and testing and makes them available to the class.
+
+        Args:
+            model: PyTorch model
+            loader: list of torch.utils.data.DataLoader where loader[0] is the training set and loader[1] is the testing set
+            criterion: PyTorch loss function
+            optimizer: PyTorch optimizer
+            device: torch.device which is either 'cuda' or 'cpu'
+            cfg: dict which holds the configuration parameters num_epochs, batch_size, and num_classes
+        """
+        self.model = model
+        self.loader = loader
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.epochs = cfg['num_epochs']
+        self.device = device
+        self.batch = cfg['batch_size']
+        self.classes = cfg['num_classes']
+        
+        self.plot_train_accuracy = np.zeros(self.epochs)
+        self.plot_train_loss = np.zeros(self.epochs)
+        self.plot_test_accuracy = np.zeros(self.epochs)
+        self.plot_test_loss = np.zeros(self.epochs)
+
+    def test_model_no_freeze(self) -> None:
+        """ This function trains the model without freezing the parameters of in the case of transfer learning.
+            This will print the loss and accuracy of the training sets per epoch.
+        """
+        print(f'Model training: {self.model.__class__.__name__}')
+        
+        for epoch in range(self.epochs):
+            print('-- epoch '+str(epoch)) 
+            running_loss_train = 0.0
+            accuracy_train = 0.0
+            predictions = 0.0
+            total_predictions = 0.0
+            self.model.train()
+            for inputs, labels in self.loader[0]:
+                peak_images, _ = inputs
+                peak_images = peak_images.to(self.device)
+                labels = labels.to(self.device)
+
+                self.optimizer.zero_grad()
+                score = self.model(peak_images)
+                loss = self.criterion(score, labels)
+
+                loss.backward()
+                self.optimizer.step()
+                running_loss_train += loss.item()  
+                predictions = (torch.sigmoid(score) > 0.5).long()  
+                accuracy_train += (predictions == labels).float().sum()
+                total_predictions += np.prod(labels.shape)
+                
+                
+            loss_train = running_loss_train/self.batch[0]
+            self.plot_train_loss[epoch] = loss_train
+            accuracy_train /= total_predictions
+            self.plot_train_accuracy[epoch] = accuracy_train
+                
+            print(f'Train loss: {loss_train}')
+            print(f'Train accuracy: {accuracy_train}')
+        
+    def test_model_freeze(self) -> None:
+        """ This function trains the model with freezing the parameters of in the case of transfer learning.
+            This will print the loss and accuracy of the testing sets per epoch.
+            WIP
+        """
+        pass
+        
+    def train_model(self) -> None:
+        """ This function test the model and prints the loss and accuracy of the testing sets per epoch.
+        """
+        print(f'Model testing: {self.model.__class__.__name__}')
+        
+        for epoch in range(self.epochs):
+            print('-- epoch '+str(epoch)) 
+            
+            running_loss_test = 0.0
+            accuracy_test = 0.0
+            predicted = 0.0
+            total = 0.0
+            self.model.eval()
+            with torch.no_grad():
+                for inputs, labels in self.loader[1]:
+                    peak_images, _ = inputs
+                    peak_images = peak_images.to(self.device)
+                    labels = labels.to(self.device)
+
+                    score = self.model(peak_images)
+                    loss = self.criterion(score, labels)
+                    running_loss_test += loss.item()  # Convert to Python number with .item()
+                    predicted = (torch.sigmoid(score) > 0.5).long()  # Assuming 'score' is the output of your model
+                    accuracy_test += (predicted == labels).float().sum()
+                    total += np.prod(labels.shape)
+
+            loss_test = running_loss_test/self.batch[1]
+            self.plot_test_loss[epoch] = loss_test
+
+            accuracy_test /= total
+            self.plot_test_accuracy[epoch] = accuracy_test
+
+            print(f'Test loss: {loss_test}')
+            print(f'Test accuracy: {accuracy_test}')
+        
+    def plot_loss_accuracy(self) -> None:
+        """ This function plots the loss and accuracy of the training and testing sets per epoch.
+        """
+        plt.plot(range(self.epochs), self.plot_train_accuracy, marker='o', color='red')
+        plt.plot(range(self.epochs), self.plot_test_accuracy, marker='o', color='orange', linestyle='dashed')
+        plt.plot(range(self.epochs), self.plot_train_loss ,marker='o',color='blue')
+        plt.plot(range(self.epochs), self.plot_test_loss ,marker='o',color='teal',linestyle='dashed')
+        plt.grid()
+        plt.xlabel('epoch')
+        plt.ylabel('loss/accuracy')
+        plt.legend(['accuracy train','accuracy test','loss train','loss test'])
+        plt.show()
+    
+    def plot_confusion_matrix(self) -> None:
+        """ This function plots the confusion matrix of the testing set.
+        """
+        cm_test = np.zeros((self.classes,self.classes), dtype=int)
+        all_labels = []
+        all_predictions = []
+
+        with torch.no_grad():
+            for inputs, label in self.loader[1]:
+                peak_images, _ = inputs
+                peak_images = peak_images.to(self.device)
+                label = label.to(self.device)
+
+                score = self.model(peak_images).squeeze()
+
+                predictions = (torch.sigmoid(score) > 0.5).long()
+
+                all_labels.extend(label.cpu().numpy().flatten()) 
+                all_predictions.extend(predictions.cpu().numpy().flatten())
+
+        all_labels = np.array(all_labels)
+        all_predictions = np.array(all_predictions)
+
+        cm_test = confusion_matrix(all_labels, all_predictions)
+
+        plt.matshow(cm_test,cmap="Blues")
+        plt.title('Confusion matrix')
+        plt.colorbar()
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
+        plt.show()
     
