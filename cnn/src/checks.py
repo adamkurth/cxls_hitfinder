a55027b0ad28cfc6f34eff6f2a6d2a784661cef4
Adam Kurth <amkurth@asu.edu>
2024-03-26 16:05:21 -0700

    checks.py integrated with process_dirctory.py

--- cnn/src/checks.py
+++ cnn/src/checks.py
@@ -1,9 +1,7 @@
 import os
 import sys
 from glob import glob
-from pkg import u
 
-# Directory creation and validation functions
 def create_and_populate_dirs(target_path):
     made_new = False
     if not os.path.exists(target_path):
@@ -43,47 +41,20 @@ def validate_directories(base_path):
             return False
     return True
 
-# Main processing function
-def process_data(directory):
-    clen_values, photon_energy_values = [1.5, 2.5, 3.5], [6000, 7000, 8000]
-    param_matrix = u.parameter_matrix(clen_values, photon_energy_values)
-    print(param_matrix, '\n')
-
-    dataset_dict = {
-        '01': [clen_values[0], photon_energy_values[0]],
-        '02': [clen_values[0], photon_energy_values[1]],
-        '03': [clen_values[0], photon_energy_values[2]],
-        '04': [clen_values[1], photon_energy_values[0]],
-        '05': [clen_values[1], photon_energy_values[1]],
-        '06': [clen_values[1], photon_energy_values[2]],
-        '07': [clen_values[2], photon_energy_values[0]],
-        '08': [clen_values[2], photon_energy_values[1]],
-        '09': [clen_values[2], photon_energy_values[2]],
-    }
-    
-    dataset_number = input("Enter dataset number: ")
-    dataset = dataset_number.zfill(2)
-    print(f'Parameter values of dataset {dataset}: {dataset_dict[dataset]}')
-
-    clen, photon_energy = dataset_dict[dataset]
-
-    pm = u.PathManager()
-    p = u.Processor(paths=pm, dataset=dataset)
-    p.process_directory(dataset=dataset, clen=clen, photon_energy=photon_energy)
-
 def main(images_dir):
     target_dirs = ["labels", "peaks", "peaks_water_overlay", "water"]
+    
     for dir_name in target_dirs:
         create_and_populate_dirs(os.path.join(images_dir, dir_name))
     
     if validate_directories(images_dir):
-        print("Directory structure and image count verification completed successfully. Proceeding with data processing.")
-        process_data(images_dir)
+        print("Directory structure and image count verification completed successfully.")
     else:
-        print("Errors detected during directory structure and image count verification. Please resolve these issues before proceeding.")
+        print("Errors detected during directory structure and image count verification.")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print(f"Usage: {sys.argv[0]} <relative_path_to_images_directory>")
         sys.exit(1)
+    
     main(sys.argv[1])
