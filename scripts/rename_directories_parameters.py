b48adaf194cef3d69f08222ed4a9a629ed897178
Adam Kurth <amkurth@asu.edu>
2024-04-02 10:20:20 -0700

    rename script revised

--- scripts/rename_directories_parameters.py
+++ scripts/rename_directories_parameters.py
@@ -2,22 +2,25 @@ import os
 import re
 
 def extract_parameters(dir_name):
-    """Extracts parameters from directory name."""
-    match = re.match(r'master_(\d{2})_(\d+)keV_clen(\d{2})', dir_name)
+    """Extracts keV and clen values from directory name."""
+    match = re.match(r'(\d{2})_(0\.\d{2})m_(\d+)keV', dir_name)
     if match:
-        return {
-            'dataset': match.group(1),
-            'photon_energy': match.group(2),
-            'clen': match.group(3)
-        }
+        dataset, clen_meter, keV = match.groups()
+        # Map dataset numbers to clen values as described
+        clen_map = {"01": "01", "02": "01", "03": "01", 
+                    "04": "02", "05": "02", "06": "02",
+                    "07": "03", "08": "03", "09": "03"}
+        clen = clen_map.get(dataset)
+        if clen:
+            return {
+                'dataset': dataset,
+                'clen': clen,
+                'keV': keV
+            }
     return None
 
 def rename_files_in_directory(root_dir):
     for root, dirs, files in os.walk(root_dir):
-        # Skip the 'water' directory
-        if 'water' in dirs:
-            dirs.remove('water')
-
         for dir_name in dirs:
             parameters = extract_parameters(dir_name)
             if parameters:
@@ -37,15 +40,15 @@ def rename_files(dataset_dir, params):
 
 def generate_new_name(filename, params, sub_dir):
     base_name = filename.split('.')[0]
-    index = base_name.split('_')[-1]
-    if sub_dir == 'peaks':
-        new_name = f'img_{params["photon_energy"]}keV_clen{params["dataset"]}_{index}.h5'
-    elif sub_dir == 'labels':
-        new_name = f'label_img_{params["photon_energy"]}keV_clen{params["dataset"]}_{index}.h5'
-    else:  # peak_water_overlay
-        new_name = f'overlay_img_{params["photon_energy"]}keV_clen{params["dataset"]}_{index}.h5'
-    return new_name
+    index = base_name.split('_')[-1]  # Assuming index is the last part after '_'
+    new_name_format = "{prefix}_img_{keV}keV_clen{clen}_{index}.h5"
+    prefix = ''  # Default for peaks
+    if sub_dir == 'labels':
+        prefix = 'label'
+    elif sub_dir == 'peak_water_overlay':
+        prefix = 'overlay'
+    return new_name_format.format(prefix=prefix, keV=params['keV'], clen=params['clen'], index=index)
 
 # Example usage
-root_directory = '../../images/'  # Adjust to your root directory
+root_directory = '../../MASTER_COPY/'  # Adjust as per your structure
 rename_files_in_directory(root_directory)
