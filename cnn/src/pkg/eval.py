800b3c7f62a04d07cc8b05e953e0d0befddb4c87
eseverett <eseveret@asu.edu>
2024-04-03 10:54:27 -0700

    improved eval.py

--- cnn/src/pkg/eval.py
+++ cnn/src/pkg/eval.py
@@ -4,11 +4,15 @@ import torch.optim as optim
 import models as m
 
 
-class Get_Configuration_Details():
-    
+class Get_Configuration_Details:
+    """
+    This class is used to get configuration details for the different properties we are looking for. 
+    """
     def __init__(self):
-        pass
-        
+        self.threshold = None  
+        self._formatted_image_attribute = None
+        self._formatted_prediction = None
+
     def get_model(self) -> nn.Module:
         return self._model
     
@@ -24,8 +28,28 @@ class Get_Configuration_Details():
     def get_labels(self) -> list:
         return self._labels
     
-    def get_formatted_image_attributes(self, image_attribute: torch.Tensor) -> torch.Tensor:
+    def format_image_attributes(self, image_attribute: torch.Tensor) -> None:
+        if not self.attribute_mapping:  # Directly use the image attribute if no mapping is needed
+            self._formatted_image_attribute = image_attribute.reshape(-1, 1).float()
+            return
+        # Use the attribute mapping for transformation
+        holder = torch.zeros_like(image_attribute, dtype=torch.long)
+        for original_value, new_value in self.attribute_mapping.items():
+            holder[image_attribute == original_value] = new_value
+        self._formatted_image_attribute = holder
+
+    def format_prediction(self, score: torch.Tensor, threshold: float = None) -> None:
+        if threshold is not None:  # For binary classification cases
+            self._formatted_prediction = (torch.sigmoid(score) > threshold).long()
+        else:  # For multi-class cases
+            _, predicted = torch.max(score, 1)
+            self._formatted_prediction = predicted
+            
+    def get_formatted_image_attribute(self) -> torch.Tensor:
         return self._formatted_image_attribute
+    
+    def get_formatted_prediction(self) -> torch.Tensor:
+        return self._formatted_prediction
     
     
 class Peak_Detection_Configuration(Get_Configuration_Details):
@@ -42,30 +66,10 @@ class Peak_Detection_Configuration(Get_Configuration_Details):
         self._feature = "peak"
         self._classes = 2
         self._labels = ["True", "False"]
-        self._formatted_image_attribute = None
-        self._formatted_prediction = None
-        
-    def format_image_attributes(self, image_attribute: torch.Tensor) -> None:
-        """
-        This function formats the image attributes for the peak detection model to use for the loss calculation.
+        self._attribute_mapping = {}
 
-        Args:
-            image_attribute (torch.Tensor): The image attribute tensor.
-        """
-        self._formatted_image_attribute = image_attribute.reshape(-1, 1).float()
-        
-    def format_prediction(self, score: torch.tensor, threshold: float) -> None:
-        """
-        This function fomats the peak detection model score  to use for accuracy calculation.
 
-        Args:
-            score (torch.tensor): Result of the peak model prediction.
-            threshold (float): The threshold for classifying peaks. 
-        """
-        self._formatted_prediction = (torch.sigmoid(score) > self.threshold).long()
-        
-        
-        
+
 class Photon_Energy_Configuration(Get_Configuration_Details):
     """
     This class is the specific configureation for the photon energy model.
@@ -80,31 +84,13 @@ class Photon_Energy_Configuration(Get_Configuration_Details):
         self._feature = "photon_energy"
         self._classes = 3
         self._labels = [6000, 7000, 8000]
-        self._formatted_image_attribute = None
-        self._formatted_prediction = None
+        self._attribute_mapping = {
+            1: 6e3,
+            2: 7e3,
+            3: 8e3
+        }
         
-    def format_image_attributes(self, image_attribute: torch.Tensor) -> None:
-        """
-        This function formats the image attributes for the photon energy model to use for the loss calculation.
 
-        Args:
-            image_attribute (torch.Tensor): The image attribute tensor.
-        """
-        attribute_mapping = {6e3: 1, 7e3: 2, 8e3: 3}
-        photon_energy_holder = torch.zeros_like(image_attribute, dtype=torch.long)
-        for original_value, new_value in attribute_mapping.items():
-            photon_energy_holder[image_attribute == original_value] = new_value
-        self._formatted_image_attribute = photon_energy_holder
-        
-    def format_prediction(self, score: torch.tensor) -> None:
-        """
-        This function fomates the photon energy model score to use for accuracy calculation.
-
-        Args:
-            score (torch.tensor): Result of the photon energy model prediction.
-        """
-        _, predicted = torch.max(score, 1)
-        self._formatted_prediction = predicted
         
         
 class Camera_Length_Configureation(Get_Configuration_Details):
@@ -121,29 +107,11 @@ class Camera_Length_Configureation(Get_Configuration_Details):
         self._feature = "clen"
         self._classes = 3
         self._labels = [1.5, 2.5, 3.5]
-        self._formatted_image_attribute = None
-        self._formatted_prediction = None
-        
-    def format_image_attributes(self, image_attribute: torch.Tensor) -> None:
-        """
-        This function formats the image attributes for the camera length model to use for the loss calculation.
+        self.attribute_mapping = {
+            1: 1.5,
+            2: 2.5,
+            3: 3.5
+        }
+            
 
-        Args:
-            image_attribute (torch.Tensor): The image attribute tensor.
-        """
-        attribute_mapping = {1.5: 1, 2.5: 2, 3.5: 3}
-        camera_length_holder = torch.zeros_like(image_attribute, dtype=torch.long)
-        for original_value, new_value in attribute_mapping.items():
-            camera_length_holder[image_attribute == original_value] = new_value
-        self._formatted_image_attribute = camera_length_holder
         
-    def format_prediction(self, score: torch.tensor, threshold: float) -> None:
-        """
-        This fucntion formates the camera length model score to use for accuracy calculation.
-
-        Args:
-            score (torch.tensor): _description_
-            threshold (float): _description_
-        """
-        _, predicted = torch.max(score, 1)
-        self._formatted_prediction = predicted
